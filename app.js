a4d093d07bfd7050f870f764a7a1f314dba5befb
SAKHI Abdelkader <abdelkader.sakhi.pro@gmail.com>
2020-06-15 16:36:02 +0200

    update app.js en branch test

--- app.js
+++ app.js
@@ -77,50 +77,70 @@ document.addEventListener('DOMContentLoaded', () => {
 
     }
 
+     //flip your card
+     function flipCard(){
+       
+       var cardId = this.getAttribute('data-id');
+       console.log("ICI c cardid ==> "+cardId);
+              cardsChosen.push(cardArray[cardId].name);
+              // console.log("==> "+cardArray[cardId].name);
+              
+              cardsChosenId.push(cardId);
+              this.setAttribute('src', cardArray[cardId].img);
+              // console.log("cardsChosenId ==> " + cardsChosenId);
+              // console.table(cardsChosenId)
+      
+              if(cardsChosen.length === 2){
+                  setTimeout(checkForMatch, 500);
+              }
+      
+          }
+
     //check for matches
 
     function checkForMatch(){
         var cards = document.querySelectorAll('img');
+        
+        //pour masquer apres la comparaison
         const optionOneId = cardsChosenId[0];
         const optionTowId = cardsChosenId[1];
+        console.log(optionOneId +" et "+optionTowId);
+
+        //en compare les nom enregistrés dans le tableau cardsChosen
         if(cardsChosen[0] === cardsChosen[1]){
+          console.log(cardsChosen[0] +" et "+cardsChosen[1]);
+          
             alert('You found a match');
             cards[optionOneId].setAttribute('src','images/white.png');
             cards[optionTowId].setAttribute('src','images/white.png');
+            //en place les cartes compatible dans le tableau cardsWon
             cardsWon.push(cardsChosen)
+            console.log(cardsWon);
+            
         }else{
             alert('Soory, try again')
             cards[optionOneId].setAttribute('src','images/blank.png');
             cards[optionTowId].setAttribute('src','images/blank.png');
         }
+
+        //en réinistialise les tableaux cardsChosen et cardsChosenId
             cardsChosen = [];
             cardsChosenId = [];
             resultDisplay.textContent = cardsWon.length;
 
             if(cardsWon.length === cardArray.length/2){
 
-                resultDisplay.textContent = 'Congratulation! you found them all!'
+               setTimeout( resultfinal ,5000);
 
 
 
             }
     }
 
-
-    //flip your card
-    function flipCard(){
-console.log("ICI c THIS ==> "+this);
-
-        var cardId = this.getAttribute('data-id');
-        cardsChosen.push(cardArray[cardId].name);
-        cardsChosenId.push(cardId);
-        this.setAttribute('src', cardArray[cardId].img);
-
-        if(cardsChosen.length === 2){
-            setTimeout(checkForMatch, 500);
-        }
-
+    function resultfinal(){
+      return resultDisplay.textContent = 'Congratulation! you found them all!';
     }
+   
 
 
     createBoard();
